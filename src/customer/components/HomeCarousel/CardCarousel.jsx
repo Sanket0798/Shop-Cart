18cfde03b935ce6058a9db6ed54f03602032563c
Sanket-NaayaTrade <161448170+Sanket-NaayaTrade@users.noreply.github.com>
2024-07-05 12:58:36 +0530

    Images in CardCarousel has been added.

--- src/customer/components/HomeCarousel/CardCarousel.jsx
+++ src/customer/components/HomeCarousel/CardCarousel.jsx
@@ -1,43 +1,27 @@
-import React from "react";
-import AliceCarousel from "react-alice-carousel";
+import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import { CardCarouselData } from './CardCarouselData'
 import "react-alice-carousel/lib/alice-carousel.css";
 
-const responsive = {
-  0: { items: 1 },
-  568: { items: 2 },
-  1024: { items: 3 },
-};
 
-const items = [
-  <div className="item" data-value="1">
-    1
-  </div>,
-  <div className="item" data-value="2">
-    2
-  </div>,
-  <div className="item" data-value="3">
-    3
-  </div>,
-];
+const items = CardCarouselData.map(
+  (item) => (
+    <img src={item.image} alt="" className='w-full h-auto cursor-pointer' />
+  )
+)
 
-const CardCarousel = () => (
-  <AliceCarousel
-    // mouseTracking
-    // items={items}
-    // responsive={responsive}
-    // controlsStrategy="alternate"
-    autoPlay
-    // autoPlayControls
-    autoPlayStrategy="none"
-    autoPlayInterval={1000}
-    animationDuration={1000}
-    animationType="fadeout"
-    infinite
-    touchTracking={false}
-    disableDotsControls
-    disableButtonsControls
-    items={items}
-  />
-);
+const CardCarousel = () => {
 
-export default CardCarousel;
+  return (
+    <AliceCarousel
+      autoPlay
+      autoPlayInterval={2000}
+      infinite
+      touchTracking={false}
+      disableButtonsControls  
+      items={items}
+    />
+  )
+}
+
+export default CardCarousel
